e6cf277ca6b6e5cfb18dc0afb020ef0a750507f5
Flagohuvelle <leandrogabrielgarcia@gmail.com>
2018-06-20 18:37:37 -0300

    email funcionando

--- contact/config.php
+++ contact/config.php
@@ -1,4 +1,4 @@
 <?php
 // To
-define("WEBMASTER_EMAIL", 'libreria2001@libreria2001.com.ar');
+define("WEBMASTER_EMAIL", 'contacto@libreria2001.com.ar');
 ?>
--- contact/contact.php
+++ contact/contact.php
@@ -4,8 +4,8 @@ Credits: Bit Repository
 URL: http://www.bitrepository.com/
 */
 
-include 'config.php';
-
+require 'PHPMailerAutoload.php';
+require("phpmailer/class.phpmailer.php");
 error_reporting (E_ALL ^ E_NOTICE);
 
 $post = (!empty($_POST)) ? true : false;
@@ -22,22 +22,57 @@ $message = stripslashes($_POST['message']);
 $error = '';
 
 
+try {
+    if(!$error){
+    $mail = new PHPMailer(); 
+    //Server settings
+    //$mail->SMTPDebug = 4;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'mail.libreria2001.com.ar';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'contacto@libreria2001.com.ar';                 // SMTP username
+    $mail->Password = 'Libreria14031963';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+    //587 default
+    //Recipients
+    $mail->setFrom('contacto@libreria2001.com.ar');
+	$mail->addAddress('libreria2001@hotmail.com');     // Add a recipient
+    $mail->addReplyTo($email);
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
 
-if(!$error)
-{
-$mail = mail(WEBMASTER_EMAIL, $subject, $message,
-     "From: ".$name." <".$email.">\r\n"
-    ."Reply-To: ".$email."\r\n"
-    ."X-Mailer: PHP/" . phpversion());
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    
+    $user_Name        = $name;
+    $user_Message     = $message;
+    $message_Body = "<strong>Nombre: </strong>". $user_Name ."<br>";
+    $message_Body .= "<strong>Email: </strong>". $email ."<br>";
+    $message_Body .= "<strong>Asunto: </strong>". $subject ."<br>";
 
-if($mail)
-{
-echo 'OK';
+    $message_Body .= "<strong>Mensaje: </strong>". $user_Message ."<br>";
+    
+    $mail->Subject = $subject;
+    $mail->Body    = $message_Body;
+    $mail->AltBody = $message_Body;
+
+    if($mail->send()) {
+
+	// Email has sent successfully, echo a success page.
+
+    echo 'OK';
+
+    } else {
+        echo 'ERROR!';
+    }
 }
-
+} catch (Exception $e) {
+    echo 'ERROR!';
 }
-
-
 }
 ?>
